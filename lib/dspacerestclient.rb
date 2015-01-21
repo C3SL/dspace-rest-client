13e4d198fd82bf20f748ad27ea699393cd3cddc1
Bruno Nocera Zanette <bnz07@inf.ufpr.br>
2015-01-20 22:43:22 -0200

    First implementation of basic methods
    Not tested yet!!!

--- lib/dspacerestclient.rb
+++ lib/dspacerestclient.rb
@@ -1,15 +1,33 @@
-class DspaceRestClient
+require File.dirname(__FILE__) + '/dspacerestclient/request'
 
-  def self.login
+module DspaceRestClient
+
+  attr_reader :dspaceurl, :headers, :cookies, :request
+
+  def self.login (username, password)
+    action = '/login'
+    data = JSON.generate({:email=>username,:password=>password})
+    response = self.post(:action => action, :data => data)
+    @token = {:'rest-dspace-token' => @response}
+    puts response
   end
 
   def self.logout
-  end
-
-  def self.status
+    action = '/logout'
+    response = self.post(:action => action)
+    puts response
   end
 
   def self.test
+    action = '/test'
+    response = self.post(:action => action)
+    puts response
   end
-  
+
+  def self.status
+    action = '/status'
+    response = self.post(:action => action)
+    puts response
+  end
+
 end
