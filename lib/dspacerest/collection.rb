80f98af40edd9db746fe28b8a45f5d23ebfbe692
Bruno Nocera Zanette <bnz07@inf.ufpr.br>
2015-02-08 04:05:01 -0200

    Major update: Attrs, New requests, to_h function

--- lib/dspacerest/collection.rb
+++ lib/dspacerest/collection.rb
@@ -2,22 +2,52 @@ module DSpaceRest
 
   class Collection
 
-    attr_accessor :handle, :id, :name, :type,
-                  :copyrightText, :numberItems, :introductoryText,
-                  :shortDescription, :sidebarText
+    attr_accessor :name, :logo, :license, :copyrightText,
+                  :introductoryText, :shortDescription, :sidebarText
+
+    attr_reader   :id, :handle, :type, :link, :parentCommunity,
+                  :parentCommunityList, :items,
+                  :numberItems, :subcommunities, :collections, :expand
 
     def initialize args, request
-      @handle = args['handle']
       @id = args['id']
       @name = args['name']
+      @handle = args['handle']
       @type = args['type']
+      @link = args['link']
+      @logo = args['logo']
+      @parentCommunity = args['parentCommunity']
+      @parentCommunityList = args['parentCommunityList']
+      @items = args['items']
+      @license = args['license']
       @copyrightText = args['copyrightText']
-      @numberItems = args['numberItems']
       @introductoryText = args['introductoryText']
       @shortDescription = args['shortDescription']
       @sidebarText = args['sidebarText']
-
+      @numberItems = args['countItems']
+      @expand = args['expand']
       @request = request
+    end
+
+    def to_h
+      h = Hash.new
+      h["id"] = @id
+      h["name"] = @name
+      h["handle"] = @handle
+      h["type"] = @type
+      h["link"] = @link
+      h["logo"] = @logo
+      h["parentCommunity"] = @parentCommunity
+      h["parentCommunityList"] = @parentCommunityList
+      h["items"] = @items
+      h["license"] = @license
+      h["copyrightText"] = @copyrightText
+      h["introductoryText"] = @introductoryText
+      h["shortDescription"] = @shortDescription
+      h["sidebarText"] = @sidebarText
+      h["numberItems"] = @numberItems
+      h["expand"] = @expand
+      h
     end
 
     def self.get_by_id(id, request)
@@ -44,13 +74,14 @@ module DSpaceRest
     end
 
     def post_item(item)
-      array_metadata = []
-      item.metadata.each_value do |m|
-        array_metadata << m.to_h
-      end
-      rqst = JSON.generate({"metadata" => array_metadata})
 
-      response = @request["/collections/#{id}/items"].post rqst
+      form_array = []
+      item.metadata.each_value do |m|
+        form_array << m.to_h
+      end
+      form = JSON.generate({"metadata" => form_array})
+
+      response = @request["/collections/#{id}/items"].post form
 
       Item.new(JSON.parse(response), @request)
     end
