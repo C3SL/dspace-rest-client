33482ea5f3c7674cc864639a584f430119e88188
Bruno Nocera Zanette <bnz07@inf.ufpr.br>
2015-02-08 04:03:06 -0200

    Major update: Attrs, New requests, to_h function

--- lib/dspacerest/community.rb
+++ lib/dspacerest/community.rb
@@ -2,22 +2,49 @@ module DSpaceRest
 
   class Community
 
-    attr_accessor :handle, :id, :name, :type,
-                  :copyrightText, :countItems, :introductoryText,
-                  :shortDescription, :sidebarText
+    attr_accessor :name, :logo, :copyrightText,
+                  :introductoryText, :shortDescription, :sidebarText
+
+    attr_reader   :id, :handle, :type, :link, :parentCommunity,
+                  :countItems, :subcommunities, :collections, :expand
 
     def initialize args, request
-      @handle = args['handle']
       @id = args['id']
       @name = args['name']
+      @handle = args['handle']
       @type = args['type']
+      @link = args['link']
+      @logo = args['logo']
+      @parentCommunity = args['parentCommunity']
       @copyrightText = args['copyrightText']
-      @countItems = args['countItems']
       @introductoryText = args['introductoryText']
       @shortDescription = args['shortDescription']
       @sidebarText = args['sidebarText']
-
+      @countItems = args['countItems']
+      @subcommunities = args['subcommunities']
+      @collections = args['collections']
+      @expand = args['expand']
       @request = request
+    end
+
+    def to_h
+      h = Hash.new
+      h["handle"] = @handle
+      h["name"] = @name
+      h["id"] = @id
+      h["type"] = @type
+      h["link"] = @link
+      h["logo"] = @logo
+      h["parentCommunity"] = @parentCommunity
+      h["copyrightText"] = @copyrightText
+      h["introductoryText"] = @introductoryText
+      h["shortDescription"] = @shortDescription
+      h["sidebarText"] = @sidebarText
+      h["countItems"] = @countItems
+      h["subcommunities"] = @subcommunities
+      h["collections"] = @collections
+      h["expand"] = @expand
+      h
     end
 
     def self.get_by_id(id, request)
@@ -34,7 +61,16 @@ module DSpaceRest
       communities
     end
 
-    def sub_communities
+    def self.get_all_top(request)
+      response = @request["/communities/top-communities"].get
+      communities = []
+      JSON.parse(response).each do |comm|
+        communities << Community.new(comm, @request)
+      end
+      communities
+    end
+
+    def get_subcommunities
       response = @request["/communities/#{id}/communities"].get
       communities = []
       JSON.parse(response).each do |comm|
@@ -50,6 +86,36 @@ module DSpaceRest
         collections << Collection.new(coll, @request)
       end
       collections
+    end
+
+    def post_community
+      form = JSON.generate(self.to_h)
+      response = @request["/communities"].post form
+
+      response_hash = JSON.parse(response)
+
+      @handle = response_hash["handle"]
+      @id = response_hash["id"]
+      @type = response_hash["type"]
+      @link = response_hash["link"]
+      @parentCommunity = response_hash["parentCommunity"]
+      @countItems = response_hash["countItems"]
+      @subcommunities = response_hash["subcommunities"]
+      @collections = response_hash["collections"]
+
+      self
+    end
+
+    def post_subcommunity(subcommunity)
+      form = JSON.generate(subcommunity.to_h)
+      response = @request["/communities/#{id}/communities"].post form
+      Community.new(JSON.parse(response))
+    end
+
+    def post_collection(collection)
+      form = JSON.generate(collection.to_h)
+      response = @request["/communities/#{id}/collections"].post form
+      Collection.new(JSON.parse(response), @request)
     end
 
   end
