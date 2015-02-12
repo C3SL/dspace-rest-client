200d4ab5fc4f63b3ccd4116b64d0f24948a192be
Bruno Nocera Zanette <bnz07@inf.ufpr.br>
2015-02-12 20:56:43 -0200

    Rearrange Attrs & New to_h method

--- lib/dspacerest/bitstream.rb
+++ lib/dspacerest/bitstream.rb
@@ -2,30 +2,54 @@ module DSpaceRest
 
   class Bitstream
 
-    attr_accessor :id, :name, :type, :bundleName,
-                  :checkSum, :description, :format, :mimeType,
-                  :retrieveLink, :sequenceId, :sizeBytes
+    attr_accessor :name, :bundleName,
+                  :description, :format, :mimeType
+
+    attr_reader   :id, :type, :link, :sizeBytes,
+                  :parentObject, :retrieveLink, :checkSum,
+                  :sequenceId, :policies
 
     def initialize args, request
-      @id = args['id']
-      @name = args['name']
-      @type = args['type']
-      @bundleName = args['bundleName']
-      @checkSum = args['checkSum']
-      @description = args['description']
-      @format = args['format']
-      @mimeType = args['mimeType']
-      @retrieveLink = args['retrieveLink']
-      @sequenceId = args['sequenceId']
-      @sizeBytes = args['sizeBytes']
-
+      @id = args['id'] || ""
+      @name = args['name'] || ""
+      @type = args['type'] || ""
+      @link = args['link'] || ""
+      @bundleName = args['bundleName'] || ""
+      @description = args['description'] || ""
+      @format = args['format'] || ""
+      @mimeType = args['mimeType'] || ""
+      @sizeBytes = args['sizeBytes'] || ""
+      @parentObject = args['parentObject'] || ""
+      @retrieveLink = args['retrieveLink'] || ""
+      @checkSum = args['checkSum'] || ""
+      @sequenceId = args['sequenceId'] || ""
+      @policies = args['policies'] || ""
+      @expand = args['expand'] || ""
       @request = request
     end
 
+    def to_h
+      h = Hash.new
+      h['id'] = @id
+      h['name'] = @name
+      h['type'] = @type
+      h['link'] = @link
+      h['bundleName'] = @bundleName
+      h['description'] = @description
+      h['format'] = @format
+      h['mimeType'] = @mimeType
+      h['sizeBytes'] = @sizeBytes
+      h['parentObject'] = @parentObject
+      h['retrieveLink'] = @retrieveLink
+      h['checkSum'] = @checkSum
+      h['sequenceId'] = @sequenceId
+      h['policies'] = @policies
+      h['expand'] = @expand
+      h
+    end
 
     def self.get_by_id(id, request)
       response = request["/bitstreams/#{id}"].get
-      puts response
       Bitstream.new(JSON.parse(response), request)
     end
 
@@ -47,15 +71,9 @@ module DSpaceRest
     end
 
     def put_metadata
-
-      array_metadata = {}
-      array_metadata["name"] = @name
-      array_metadata["bundleName"] = @bundleName
-      array_metadata["description"] = @description
-
-      rqst = JSON.generate(array_metadata)
-
-      response = @request["/bitstreams/#{id}"].put rqst
+      valid_keys=['name', 'description','sequenceId']
+      form = JSON.generate(self.to_h.select { |k,v| valid_keys.include? k })
+      response = @request["/bitstreams/#{id}"].put form
     end
 
   end
