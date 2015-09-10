45863cdc4d5a1bb0bb506133dd5c4600f868c44a
Bruno Nocera Zanette <bnzanette@inf.ufpr.br>
2015-09-10 18:04:26 -0300

    Change install script to get version from gemspec

--- install.sh
+++ install.sh
@@ -10,4 +10,4 @@ fi
 name=$(grep -m 1 "s\.name" ${gemspec_file} | cut -d "=" -f2 | sed "s#\ ##g; s#'##g")
 version=$(grep -m 1 "s\.version" ${gemspec_file} | cut -d "=" -f2 | sed "s#\ ##g; s#'##g")
 
-gem build ${gemspec_file} && gem install --local ${name}-${version}.gem
+gem build ${gemspec_file} && gem install ${name}-${version}.gem
