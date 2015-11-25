a56d3e8679a5437f442f4dae4b85e0825ebc2a94
Mauricio Giacomini Girardello <mauriciogiacomini4@gmail.com>
2015-11-25 10:32:18 -0200

    adding top-communities test

--- spec/lib/dspace/resources/community_resource_spec.rb
+++ spec/lib/dspace/resources/community_resource_spec.rb
@@ -6,6 +6,10 @@ RSpec.describe Dspace::Resources::CommunityResource, resource_kit: true do
     expect(Dspace::Resources::CommunityResource).to have_action(:all).that_handles(200).at_path('/communities')
   end
 
+  it 'has an top communities action' do
+    expect(Dspace::Resources::CommunityResource).to have_action(:top_communities).that_handles(200).at_path('/communities/top-communities')
+  end
+
   #it 'handles a 201 with response body' do
   #  expect(MyResourceClass).to handle_response(:create).with(status: 201, body: '{"users":[]}') do |handled|
   #    expect(handled).to all(be_kind_of(User))
