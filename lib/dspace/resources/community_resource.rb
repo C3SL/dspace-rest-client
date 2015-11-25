56ceffd0ef08104ec791429b3a80f10568b95541
Mauricio Giacomini Girardello <mauriciogiacomini4@gmail.com>
2015-11-25 10:47:29 -0200

    add find community test

--- lib/dspace/resources/community_resource.rb
+++ lib/dspace/resources/community_resource.rb
@@ -2,6 +2,12 @@ module Dspace
   module Resources
     class CommunityResource < ResourceKit::Resource
 
+      # :all GET /communities - Return an array of all communities in DSpace.
+      # :top_communities GET /communities/ top-communities - Returns an array of all top-leve communities in DSpace.
+      # :find GET /communities/{communityId} - Returns a community with the specified ID.
+      # :collections GET /communities/{communityId}/collections - Returns an array of collections of the specified community.
+      # :subcommunities GET /communities/{communityId}/communities - Returns an array of subcommunities of the specified community.
+
       resources do
         action :all, 'GET /communities' do
           handler(200) do |response|
--- spec/lib/dspace/resources/community_resource_spec.rb
+++ spec/lib/dspace/resources/community_resource_spec.rb
@@ -10,6 +10,10 @@ RSpec.describe Dspace::Resources::CommunityResource, resource_kit: true do
     expect(Dspace::Resources::CommunityResource).to have_action(:top_communities).that_handles(200).at_path('/communities/top-communities')
   end
 
+  it 'has find community action' do
+    expect(Dspace::Resources::CommunityResource).to have_action(:find).that_handles(200).at_path('/communities/:id')
+  end
+
   #it 'handles a 201 with response body' do
   #  expect(MyResourceClass).to handle_response(:create).with(status: 201, body: '{"users":[]}') do |handled|
   #    expect(handled).to all(be_kind_of(User))
