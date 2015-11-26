689f3c26f3dda37172295c8bd276537237218ab1
Lucas Ernesto Kindinger <lek@dinf.ufpr.br>
2015-11-26 10:43:16 -0200

    Added tests for collections

--- lib/dspace.rb
+++ lib/dspace.rb
@@ -1,8 +1,10 @@
 require "forwardable"
 require "json"
 require "resource_kit"
+
 require_relative "dspace/version"
 require_relative "dspace/resources/community_resource"
+require_relative "dspace/resources/collection_resource"
 require_relative "dspace/builders/hash_builder"
 require_relative "dspace/builders/model_builder"
 require_relative "dspace/community"
