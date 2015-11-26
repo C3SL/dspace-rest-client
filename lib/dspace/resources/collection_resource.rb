3d3060350ff6f6b117d065ab8265d94359afea7a
Mauricio Giacomini Girardello <mauriciogiacomini4@gmail.com>
2015-11-26 10:35:30 -0200

    adding collection resources

---
+++ lib/dspace/resources/collection_resource.rb
@@ -0,0 +1,18 @@
+module Dspace
+  module Resources
+    class CollectionResource < ResourceKit::Resource
+
+      resources do
+        default_handler(401) { raise NotAuthorizedError, 'This request requires authentication' }
+
+        action :all, 'GET /rest/collections' do
+          query_keys :expand
+          handler(200) do |response|
+            Dspace::Builders::ModelBuilder.build_collections(JSON.parse(response.body))
+          end
+        end
+
+      end
+    end
+  end
+end
