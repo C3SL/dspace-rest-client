d6fea959e3c0078d5d8938e3bef1313ae579246c
Mauricio Giacomini Girardello <mauriciogiacomini4@gmail.com>
2015-11-26 10:39:43 -0200

    adding find collection action

--- lib/dspace/resources/collection_resource.rb
+++ lib/dspace/resources/collection_resource.rb
@@ -12,6 +12,13 @@ module Dspace
           end
         end
 
+        action :find, 'GET /rest/collections/:id' do
+          query_keys :expand
+          handler(200) do |response|
+            Dspace::Collection.new(JSON.parse(response.body))
+          end
+        end
+
       end
     end
   end
