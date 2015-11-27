35b301279f2010d5760dabcd286ae0bbc5b943e0
Mauricio Giacomini Girardello <mauriciogiacomini4@gmail.com>
2015-11-27 11:56:40 -0200

    adding bitstream resource

---
+++ lib/dspace/resources/bitstream_resource.rb
@@ -0,0 +1,65 @@
+module Dspace
+  module Resources
+    class BitstreamResource < ResourceKit::Resource
+
+
+      # PUT /bitstreams/{bitstream id}/data - Update data/file of bitstream. You must put the data
+      # PUT /bitstreams/{bitstream id} - Update metadata of bitstream. You must put a Bitstream, does not alter the file/data
+
+
+      resources do
+        default_handler(401) { raise NotAuthorizedError, 'This request requires authentication' }
+
+        action :all, 'GET /rest/bitstreams' do
+          query_keys :expand, :limit, :offset
+          handler(200) do |response|
+            Dspace::Builders::ModelBuilder.build_items(JSON.parse(response.body))
+          end
+        end
+
+        action :find, 'GET /rest/bitstreams/:id' do
+          query_keys :expand
+          handler(200) do |response|
+            Dspace::Bitstream.new(JSON.parse(response.body))
+          end
+        end
+
+        action :policy, 'GET /rest/bitstreams/:id/policy' do
+          handler(200) do |response|
+            Dspace::Policy.new(JSON.parse(response.body))
+          end
+        end
+
+        #action :retrive, 'GET /rest/bitstreams/:id/retrieve' do
+        #  handler(200) do |response|
+        #    tmpfile = Tempfile.new([filename, extension])
+        #  end
+        #end
+
+        action :delete, 'DELETE /rest/bitstreams/:id' do
+          handler(200, 201, 204) { |response| true }
+        end
+
+        action :delete_policy, 'DELETE /rest/bitstreams/:id/policy/:policy_id' do
+          handler(200, 201, 204) { |response| true }
+        end
+
+        action :add_policy, 'POST /rest/bitstreams/:id/policy ' do
+          body { |object| JSON.generate(object.to_h) }
+          handler(200, 201) { |response| true }
+        end
+
+        action :update, 'PUT /rest/bitstreams/:id ' do
+          body { |object| JSON.generate(object.to_h) }
+          handler(200, 201) { |response| true }
+        end
+
+       # action :update_data, 'PUT /rest/bitstreams/:id/data' do
+       #   body { |object| JSON.generate(object.to_h) }
+        #  handler(200, 201) { |response| true }
+       # end
+
+      end
+    end
+  end
+end
