0c5cf2d1f926bb1410db730f2541e6ede1bf2b78
Mateus Rambo Strey <mars11@inf.ufpr.br>
2016-05-02 10:21:46 -0300

    fix filename sanitize method

--- lib/dspace/builders/tempfile_builder.rb
+++ lib/dspace/builders/tempfile_builder.rb
@@ -3,22 +3,19 @@ module Dspace
     module TempfileBuilder
 
       def self.build(filename, contents = nil, bitstreams_path = '/tmp')
-        file = Tempfile.new([sanitize_filename(filename), File.extname(filename)], bitstreams_path, encoding: 'ascii-8bit').tap do |f|
+        Tempfile.new([sanitize_filename(filename), File.extname(filename)], bitstreams_path, encoding: 'ascii-8bit').tap do |f|
           f.write contents
           f.close
         end
       end
 
-      private
-
       def self.sanitize_filename(filename)
-        filename.strip do |name|
-          # NOTE: File.basename doesn't work right with Windows paths on Unix
-          # get only the filename, not the whole path
-          name.gsub!(/^.*(\\|\/)/, '')
-          # Strip out the non-ascii character
-          name.gsub!(/[^0-9A-Za-z.\-]/, '_')
-        end
+        filename.strip!
+        # NOTE: File.basename doesn't work right with Windows paths on Unix
+        # get only the filename, not the whole path
+        filename.gsub!(/^.*(\\|\/)/, '')
+        # Strip out the non-ascii character
+        filename.gsub!(/[^0-9A-Za-z.\-]/, '_')
       end
 
     end
